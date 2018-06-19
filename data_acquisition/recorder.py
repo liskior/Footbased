96f24243d2a497d094b1cf232f8b2e5fd3d42a0d
Alp <uoehw@student.kit.edu>
2018-06-19 22:20:08 +0200

    initial commit for recorder

---
+++ data_acquisition/recorder.py
@@ -0,0 +1,20 @@
+from random import shuffle
+from os import system
+from time import sleep
+
+MOVEMENTS = ['TOE RAISE', 'HEEL RAISE', 'BACKWARD SLIDE']
+TIMES_PER_MOVEMENT = 3
+
+to_be_recorded = MOVEMENTS * TIMES_PER_MOVEMENT
+shuffle(to_be_recorded)
+
+for movement in to_be_recorded:
+    system('echo "("' + movement + ' >> /tmp/current_recording.txt')
+    system('clear')
+    print movement
+    for i in range(5, 0, -1):
+        print i 
+        sleep(1)
+    system('echo ' + movement + ' ")" >> /tmp/current_recording.txt')
+    print 'Recorded. Next move...'
+    sleep(2)
