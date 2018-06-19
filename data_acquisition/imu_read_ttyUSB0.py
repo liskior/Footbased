70b4651c104e80fd981dbb71e0ce426bfe1bdd52
Alp <uoehw@student.kit.edu>
2018-06-19 14:35:54 +0200

    re-add imu_read.py for /dev/ttyUSB0 (no bluetooth)

---
+++ data_acquisition/imu_read_ttyUSB0.py
@@ -0,0 +1,9 @@
+# Works on Ubuntu. Communicates over USB cable.
+
+import serial
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = '/dev/ttyUSB0'
+ser.open()
+while True:
+    print ser.readline() 
