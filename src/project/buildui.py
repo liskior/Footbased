df4a1b5b6a55627a2e4c6d038ad23b67336f56b2
Anastasia  Kryzhanovskaya <liskior@gmail.com>
2018-07-19 15:20:10 +0200

    für alp

--- src/project/buildui.py
+++ src/project/buildui.py
@@ -2,41 +2,37 @@ from pyxley.charts.mg import Figure, Histogram, LineChart, ScatterPlot
 from pyxley.filters import SelectButton
 from pyxley import UILayout, register_layouts
 import pandas as pd
+from os import path
 
 from collections import OrderedDict
 from flask import request, jsonify
 
 
-def create_line_plot(df):
+def create_line_plot(df, i, name):
     """ create a mg line plot
 
         Args:
             df (pandas.DataFrame): data to plot
     """
-    fig = Figure("/mg/line_plot/", "mg_line_plot")
+    fig = Figure("/mg/line_plot" + str(i), "mg_line_plot")
     fig.graphics.transition_on_update(True)
     fig.graphics.animate_on_load()
-    fig.graphics.x_rug()
-    fig.graphics.y_rug()
     fig.graphics.point_size(2.0)
-    fig.layout.set_size(width=450, height=210)
+    fig.layout.set_size(width=450 + 10*i, height=210)
     fig.layout.set_margin(left=40, right=40)
     fig.axes.set_min_y_from_data(True)
     fig.axes.set_inflator(1.005)
     fig.axes.set_xticks_count(3)
     return LineChart(df=df, figure=fig, x="Year", y=["value"],
-                     init_params={"Data": "Abschlussprüfungsleistungen"},
-                     timeseries=True,
-                     title="Boring Data of boring firm")
+                     init_params={"Data": name},
+                     timeseries=True)
 
 
 def make_mg_layout(filename):
     # load a dataframe
     df = pd.read_csv(filename)
-
     # Make a UI
     ui = UILayout("FilterChart")
-
     # Make a Button
     cols = [c for c in df.columns if c != "Year"]
     btn = SelectButton("Abschlussprüfungsleistungen", cols, "Data", "Abschlussprüfungsleistungen")
@@ -49,7 +45,25 @@ def make_mg_layout(filename):
     _stack = _stack.rename(columns={"level_1": "Data", 0: "value"})
 
     # Make a Figure, add some settings, make a line plot
-    ui.add_chart(create_line_plot(_stack))
+    ui.add_chart(create_line_plot(_stack, i=0, name="Abschlussprüfungsleistungen"))
+
+    here = path.abspath(path.dirname(__file__))
+
+    df1 = pd.read_csv(here + "/file")
+    # Make a Button
+    cols1 = [c for c in df1.columns if c != "Year"]
+    btn1 = SelectButton("Professionals", cols1, "Data", "Professionals")
+
+    # add the button to the UI
+    ui.add_filter(btn1)
+    _stack1 = df1.set_index("Year").stack().reset_index()
+    _stack1 = _stack1.rename(columns={"level_1": "Data", 0: "value"})
+
+    # Make a Figure, add some settings, make a line plot
+    ui.add_chart(create_line_plot(_stack1, i=1, name="Professionals"))
+
+    # stack the dataframe
+
 
     return ui
 
