360def3b1ed7499ef3a824b071cc0820353c15e3
Anastasia  Kryzhanovskaya <liskior@gmail.com>
2018-07-19 17:02:17 +0200

    endpoint gui

--- src/mapping.py
+++ src/mapping.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
 class Mapping(object):
-    plots = ["mg_line_plot", "mg_histogram", "mg_scatter"]
 
     active = 0
     __list = [False, False, False]
@@ -28,31 +27,28 @@ class Mapping(object):
         self.show(2)
 
     def open_list(self, id):
+        if self.active != id:
+            return
         print(self.__driver)
         python_button = self.__driver.find_elements_by_xpath("//button[@id='filter_" + str(id) + "']")[0]
         python_button.click()
         self.__list[id] = True
 
     def click(self, n, id):
+        if self.active != id:
+            return
         if not self.__list[id]:
             self.open_list(id)
 
         self.__list[id] = False
-        python_button1 = self.__driver.find_elements_by_xpath("//li[@role='presentation']")[n]
+        python_button1 = self.__driver.find_elements_by_xpath("//li[@role='presentation']")[self.crutch[self.active] + n]
         python_button1.click()
 
     def __del__(self):
         self.__driver.quit()
 
-    def next(self):
-        script = "document.getElementById('" + self.plots[self.active] + "').firstChild.style.height='0'"
-        self.__driver.execute_script(script)
-        self.active = (self.active + 1) % 3
-        script = "document.getElementById('" + self.plots[self.active] + "').firstChild.style.height='200'"
-        self.__driver.execute_script(script)
-
     def choose(self, n):
-        python_button = self.__driver.find_elements_by_xpath("//li[@role='presentation']")[n]
+        python_button = self.__driver.find_elements_by_xpath("//li[@role='presentation']")[self.crutch[self.active] + n]
         hover = ActionChains(self.__driver).move_to_element(python_button)
         hover.perform()
 
@@ -65,3 +61,4 @@ class Mapping(object):
                 script = "document.getElementsByClassName('btn-group')[" + str(j) + "].style.display='inline-block'"
                 self.__driver.execute_script(script)
         self.click(self.crutch[i] + 1, i)
+        self.active = i
