a96441d4350d161c5a7c9b182beebe2a385191be
Anastasia  Kryzhanovskaya <liskior@gmail.com>
2018-07-19 14:40:18 +0200

    gui fix

--- src/project/buildui.py
+++ src/project/buildui.py
@@ -16,47 +16,18 @@ def create_line_plot(df):
     fig = Figure("/mg/line_plot/", "mg_line_plot")
     fig.graphics.transition_on_update(True)
     fig.graphics.animate_on_load()
-    fig.layout.set_size(width=450, height=200)
+    fig.graphics.x_rug()
+    fig.graphics.y_rug()
+    fig.graphics.point_size(2.0)
+    fig.layout.set_size(width=450, height=210)
     fig.layout.set_margin(left=40, right=40)
-    return LineChart(df, fig, "Date", ["value"],
-                     init_params={"Data": "Eaten in a dream spiders"}, timeseries=True)
-
-
-def create_histogram(df):
-    """ create a mg line plot
-
-        Args:
-            df (pandas.DataFrame): data to plot
-    """
-    fig = Figure("/mg/histogram/", "mg_histogram")
-    fig.layout.set_size(width=450, height=200)
-    fig.layout.set_margin(left=40, right=40)
-    fig.graphics.animate_on_load()
-
-    # Make a histogram with 20 bins
-    return Histogram(df, fig, "value", 20, init_params={"Data": "Eaten in a dream spiders"})
-
-
-def create_scatterplot(df):
-    """ create a mg line plot
-
-        Args:
-            df (pandas.DataFrame): data to plot
-    """
-    fig = Figure("/mg/scatter/", "mg_scatter")
-    fig.layout.set_size(width=450, height=200)
-    fig.layout.set_margin(left=40, right=40)
-    fig.graphics.animate_on_load()
-
-    init_params = {"Data": "Eaten in a dream spiders"}
-
-    def get_data():
-        y = request.args.get("Data", "Eaten in a dream spiders")
-        return jsonify(ScatterPlot.to_json(df, "Eaten in a dream spiders", y))
-
-    # Make a histogram with 20 bins
-    return ScatterPlot(df, fig, "Eaten in a dream spiders", "Spiders killed by Harry",
-                       init_params={}, route_func=get_data)
+    fig.axes.set_min_y_from_data(True)
+    fig.axes.set_inflator(1.005)
+    fig.axes.set_xticks_count(3)
+    return LineChart(df=df, figure=fig, x="Year", y=["value"],
+                     init_params={"Data": "Abschlussprüfungsleistungen"},
+                     timeseries=True,
+                     title="Boring Data of boring firm")
 
 
 def make_mg_layout(filename):
@@ -67,23 +38,18 @@ def make_mg_layout(filename):
     ui = UILayout("FilterChart")
 
     # Make a Button
-    cols = [c for c in df.columns if c != "Date"]
-    btn = SelectButton("Data", cols, "Data", "Eaten in a dream spiders")
-    
+    cols = [c for c in df.columns if c != "Year"]
+    btn = SelectButton("Abschlussprüfungsleistungen", cols, "Data", "Abschlussprüfungsleistungen")
 
     # add the button to the UI
     ui.add_filter(btn)
 
     # stack the dataframe
-    _stack = df.set_index("Date").stack().reset_index()
+    _stack = df.set_index("Year").stack().reset_index()
     _stack = _stack.rename(columns={"level_1": "Data", 0: "value"})
 
     # Make a Figure, add some settings, make a line plot
     ui.add_chart(create_line_plot(_stack))
-    histo = create_histogram(_stack)
-    histo.__setattr__("visible", "false")
-    ui.add_chart(histo)
-    ui.add_chart(create_scatterplot(df))
 
     return ui
 
